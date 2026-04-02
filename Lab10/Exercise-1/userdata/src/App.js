b15c0fae02f90a11502b5f41b9d9c2ffb54a00ad
Pavan Mandadi <pavanmandadi09@gmail.com>
2026-04-02 21:52:18 +0530

    Exercise-1

---
+++ Lab10/Exercise-1/userdata/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UserForm from "./UserForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>User Registration Form</h1>
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
